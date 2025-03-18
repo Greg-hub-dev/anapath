07f1bdc62eb0fa101f6ae488f1190e59c4169f5a
Doron Abergel <doronabergel@hotmail.com>
2025-03-18 17:37:17 +0100

    mise a jour model_tx

--- anapath/Logic/model_tx.py
+++ anapath/Logic/model_tx.py
@@ -4,28 +4,45 @@ import time
 from colorama import Fore, Style
 from typing import Tuple
 from tensorflow import keras
+import tensorflow as tf
 from keras import Model, Sequential, layers, regularizers, optimizers
 from keras.callbacks import EarlyStopping
 
 
 ### A ADAPTER COMPLETEMENT
 
-
 def initialize_model(input_shape: tuple) -> Model:
     """
     Initialize the Neural Network with random weights
     """
-    reg = regularizers.l1_l2(l2=0.005)
+    model = tf.keras.Sequential([
+    # INPUT LAYER
+    layers.Input(shape=(input_shape)),
 
-    model = Sequential()
-    model.add(layers.Input(shape=input_shape))
-    model.add(layers.Dense(100, activation="relu", kernel_regularizer=reg))
-    model.add(layers.BatchNormalization(momentum=0.9))
-    model.add(layers.Dropout(rate=0.1))
-    model.add(layers.Dense(50, activation="relu"))
-    model.add(layers.BatchNormalization(momentum=0.9))  # use momentum=0 to only use statistic of the last seen minibatch in inference mode ("short memory"). Use 1 to average statistics of all seen batch during training histories.
-    model.add(layers.Dropout(rate=0.1))
-    model.add(layers.Dense(1, activation="linear"))
+    # CONV/HIDDEN LAYERS
+    layers.Conv2D(32, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'),
+    layers.Conv2D(32, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'),
+    layers.MaxPool2D(pool_size=(2,2)),
+    #layers.Dropout(0.2),
+    #layers.Conv2D(64, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'),
+    #layers.Conv2D(64, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'),
+    ##layers.MaxPool2D(pool_size=(2,2)),
+    #layers.Dropout(0.2),
+    #layers.Conv2D(128, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'),
+    #layers.Conv2D(128, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'),
+    ##layers.MaxPool2D(pool_size=(2,2)),
+    #layers.Dropout(0.2),
+
+    ### Flattening
+    layers.Flatten(),
+
+    ### One Fully Connected layer - "Fully Connected" is equivalent to saying "Dense"
+    layers.Dense(16, activation='relu',  kernel_initializer='he_uniform'),
+    layers.Dropout(0.2),
+
+    # PREDICITVE LAYER
+    layers.Dense(1, activation='sigmoid')
+])
 
     print("✅ Model initialized")
 
@@ -36,21 +53,26 @@ def compile_model(model: Model, learning_rate=0.0005) -> Model:
     """
     Compile the Neural Network
     """
-    optimizer = optimizers.Adam(learning_rate=learning_rate)
-    model.compile(loss="mean_squared_error", optimizer=optimizer, metrics=["mae"])
+    adam_opt = optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.999)
+    model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
 
     print("✅ Model compiled")
 
     return model
 
+
+
 def train_model(
         model: Model,
-        X: np.ndarray,
-        y: np.ndarray,
-        batch_size=256,
-        patience=2,
-        validation_data=None, # overrides validation_split
-        validation_split=0.3
+        X,
+        y,
+        validation_data,
+        epochs=10,
+        batch_size=2,
+        patience=10,
     ) -> Tuple[Model, dict]:
     """
     Fit the model and return a tuple (fitted_model, history)
@@ -64,50 +86,14 @@ def train_model(
         verbose=1
     )
 
-    history = model.fit(
-        X,
-        y,
+    history_cnn = model.fit(
+        X,y,
+        batch_size=batch_size,
+        epochs=epochs,
         validation_data=validation_data,
-        validation_split=validation_split,
-        epochs=100,
-        batch_size=batch_size,
-        callbacks=[es],
-        verbose=0
-    )
+        callbacks=[es]
+        )
 
-    print(f"✅ Model trained on {len(X)} rows with min val MAE: {round(np.min(history.history['val_mae']), 2)}")
+    print(f"✅ Model trained on {len(X)} images with min val Accuracy: {round(np.min(history_cnn.history['val_accuracy']), 2)}")
 
-    return model, history
-
-
-def evaluate_model(
-        model: Model,
-        X: np.ndarray,
-        y: np.ndarray,
-        batch_size=64
-    ) -> Tuple[Model, dict]:
-    """
-    Evaluate trained model performance on the dataset
-    """
-
-    print(Fore.BLUE + f"\nEvaluating model on {len(X)} rows..." + Style.RESET_ALL)
-
-    if model is None:
-        print(f"\n❌ No model to evaluate")
-        return None
-
-    metrics = model.evaluate(
-        x=X,
-        y=y,
-        batch_size=batch_size,
-        verbose=0,
-        # callbacks=None,
-        return_dict=True
-    )
-
-    loss = metrics["loss"]
-    mae = metrics["mae"]
-
-    print(f"✅ Model evaluated, MAE: {round(mae, 2)}")
-
-    return metrics
+    return model, history_cnn
