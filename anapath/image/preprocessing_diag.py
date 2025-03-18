a310875f96f0104327453b0afd064e91b919e191
Gregoire Puscian <g.puscian@gmail.com>
2025-03-18 07:57:28 +0100

    ajout d'un modele basique dans diag

--- anapath/image/preprocessing_diag.py
+++ anapath/image/preprocessing_diag.py
@@ -6,6 +6,8 @@ import numpy as np
 import os
 from PIL import Image
 import cv2
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from anapath.params import *
 
 def load_image_anapath(type='tumor',env='train', max_image=2):
     """Charge les images dans des X et y"""
@@ -53,3 +55,49 @@ def preprocess_image(image : np.array, target_size=(256, 256), normalize=True):
         image_array = image_array.astype(np.float32) / 255.0
 
     return image_array
+
+def datagenerator(type):
+# Configuration du générateur d'augmentation
+
+    if type == 'diag':
+        datagen = ImageDataGenerator(
+            rescale = 1/255,           #Pas sur que cela soit nécesssaire car déjà scaler
+            rotation_range=20,        # Rotation aléatoire jusqu'à 20 degrés
+            width_shift_range=0.2,    # Décalage horizontal jusqu'à 20% de la largeur
+            height_shift_range=0.2,   # Décalage vertical jusqu'à 20% de la hauteur
+            #shear_range=0.2,          # Cisaillement (déformation) jusqu'à 20%
+            zoom_range=0.2,           # Zoom aléatoire entre 80% et 120%
+            horizontal_flip=True,     # Retournement horizontal aléatoire
+            fill_mode='nearest')      # Méthode pour remplir les pixels créés après transformation
+        train_generator = datagen.flow_from_directory(
+            train_path,  # Dossier parent contenant un sous-dossier par classe
+            target_size=(512, 1024),        # Redimensionnement des images
+            batch_size=1,                 # Nombre d'images par lot
+            class_mode='categorical')      # Type d'encodage des étiquettes (one-hot pour multi-classes)
+        validation_generator = datagen.flow_from_directory(
+            val_path,  # Dossier parent contenant un sous-dossier par classe
+            target_size=(512, 1024),        # Redimensionnement des images
+            batch_size=1,                 # Nombre d'images par lot
+            class_mode='categorical')      # Type d'encodage des étiquettes (one-hot pour multi-classes)
+        return datagen, train_generator, validation_generator
+    else:
+        datagen = ImageDataGenerator(
+            rescale = 1/255,           #Pas sur que cela soit nécesssaire car déjà scaler
+            rotation_range=20,        # Rotation aléatoire jusqu'à 20 degrés
+            width_shift_range=0.2,    # Décalage horizontal jusqu'à 20% de la largeur
+            height_shift_range=0.2,   # Décalage vertical jusqu'à 20% de la hauteur
+            #shear_range=0.2,          # Cisaillement (déformation) jusqu'à 20%
+            zoom_range=0.2,           # Zoom aléatoire entre 80% et 120%
+            horizontal_flip=True,     # Retournement horizontal aléatoire
+            fill_mode='nearest')      # Méthode pour remplir les pixels créés après transformation
+        train_generator = datagen.flow_from_directory(
+            train_path,  # Dossier parent contenant un sous-dossier par classe
+            target_size=(512, 1024),        # Redimensionnement des images
+            batch_size=1,                 # Nombre d'images par lot
+            class_mode='categorical')      # Type d'encodage des étiquettes (one-hot pour multi-classes)
+        validation_generator = datagen.flow_from_directory(
+            val_path,  # Dossier parent contenant un sous-dossier par classe
+            target_size=(512, 1024),        # Redimensionnement des images
+            batch_size=1,                 # Nombre d'images par lot
+            class_mode='categorical')      # Type d'encodage des étiquettes (one-hot pour multi-classes)
+    return datagen, train_generator, validation_generator
